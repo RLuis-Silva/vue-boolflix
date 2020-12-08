f84c5d91efc40c12d0bb691e2234cb33558ee404
Luis <s.ros.lui@gmail.com>
2020-12-08 16:06:57 +0100

    milestone 1

--- main.js
+++ main.js
@@ -1,35 +1,64 @@
+/**
+ * MILESTONE 1:
+ *      Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere
+ *      completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film 
+ *      che contengono ciò che ha scritto l’utente.
+ *      Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato: 
+*       - Titolo
+*       - Titolo Originale
+*       - Lingua
+*       -Voto
+*
+*/
+
+
 const app = new Vue({
     el:"#app",
-    data: {},
+    data: {
+        query:"",
+        risultati:[],
+        bandiere: ["it", "en"]
+    },
 
-    created(){
-        /**
-         * AJAX
-         */
-        //struttura metodo get() API
-        axios.get("inserisci url qui", {    //inserisci url 
-            //struttura dati
-            params: {
-                query: "tree",
-                api_key: "f976de25effb1ed1a9ddf752730695bc"
-            }
-        })     
-        .then(result=>{
-            // caso positivo
-            console.log(result.data);
-        })
-        .catch(errore =>{
-            // caso di errore
-            console.log(errore);
-        });
+    methods:{
+        // metodo "search", cerca risultati nell' API
+        search(){
+            axios.get("https://api.themoviedb.org/3/search/movie", {
+                params:{
+                    api_key: "e99307154c6dfb0b4750f6603256716d",
+                    query: this.query,
+                    language: "it-IT"
+
+                }
+            })
+                    .then(risposta => {
+                        // gestione caso di successo
+                        const ris = risposta.data.results;
+                        // console.log(ris);
+                        this.risultati = ris;
+                    })
+                    .catch(errore => {
+                        // gestione caso di errore
+                        console.log(errore);
+                    });
+        },
+
+        // conversione voto
+        getVote(vote){
+            return Math.ceil(vote / 2);
+        },
+
+        //controlla il linguaggio in entrata, 
+        // restituisce true se in bandiere contiene la lingua "language"
+        isLangFlag(language){
+            return this.bandiere.includes(language);
+        },
+
+        // prende l'immagine della bandiera
+        getFlag(language){
+            return `./img/${language}.png` 
+        }
     }
 });
 
 
-console.log("TORNOOOOOO SUBIIITOOOOOOO");
-console.log("TORNOOOOOO SUBIIITOOOOOOO");
-console.log("TORNOOOOOO SUBIIITOOOOOOO");
-console.log("TORNOOOOOO SUBIIITOOOOOOO");
-console.log("TORNOOOOOO SUBIIITOOOOOOO");
-console.log("TORNOOOOOO SUBIIITOOOOOOO");
-console.log("TORNOOOOOO SUBIIITOOOOOOO");
